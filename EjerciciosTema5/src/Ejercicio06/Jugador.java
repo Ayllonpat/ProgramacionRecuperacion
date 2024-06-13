775081e11dae65f350fd933ebcf30840081d6b57
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2024-06-13 13:42:12 +0200

    colecciones

---
+++ EjerciciosTema5/src/Ejercicio06/Jugador.java
@@ -0,0 +1,39 @@
+package Ejercicio06;
+
+public class Jugador {
+	
+	private String nombre;
+	private String posicion;
+	private int nCamiseta;
+	public Jugador(String nombre, String posicion, int nCamiseta) {
+		super();
+		this.nombre = nombre;
+		this.posicion = posicion;
+		this.nCamiseta = nCamiseta;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+	public int getnCamiseta() {
+		return nCamiseta;
+	}
+	public void setnCamiseta(int nCamiseta) {
+		this.nCamiseta = nCamiseta;
+	}
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", posicion=" + posicion + ", nCamiseta=" + nCamiseta + "]";
+	}
+	
+	
+
+}
---
+++ EjerciciosTema5/src/Ejercicio06/Principal.java
@@ -0,0 +1,29 @@
+package Ejercicio06;
+
+public class Principal {
+	
+	/*
+	 * Vamos a crear un programa que nos ayude para gestionar un equipo de fútbol. Para ello, se deben crear tres clases, una
+		llamada Jugador, otra Equipo y una Principal de prueba. Dependiendo de la forma que uséis para ordenar, puede que haga
+		falta otra clase más.
+		Los Jugadores están caracterizados por: Nombre, posición (Un string. Pueden ser central, lateral derecho, delantero
+		centro...) y el número de la camiseta.
+		Nuestra clase Equipo se caracteriza por una lista de jugadores, siendo la colección usada para almacenarlos un ArrayList.
+		En ella, se debe poder hacer lo siguiente:
+		• Añadir un nuevo jugador.
+		• Borrar un jugador de la lista buscando antes al mismo.
+		• Imprimir todos los datos de todos los jugadores de la lista usando un iterator.
+		• Buscar un jugador por nombre.
+		• Buscar todos los jugadores que tengan la misma posición, por ejemplo, todos los centrales.
+		• Modificar la posición en que juega un jugador buscando antes al mismo.
+		• Ordenar la lista por posiciones de juego.
+		Se puede usar, si se quiere, un método que cargue la lista con varios jugadores de prueba para no tener que leerlos por
+		teclado.
+		Crear un menú en la clase principal donde se pueda comprobar toda la funcionalidad del programa.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
